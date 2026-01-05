26d5006fd66de317b270eec13289f0a69df4522f
ngocphu <phupn1304@ut.edu.vn>
2026-01-05 12:03:39 +0700

    fix

--- AESP-project/backend/backend/src/main/java/com/aesp/backend/repository/AssessmentResultRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aesp.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aesp.backend.entity.AssessmentResult;
-
-public interface AssessmentResultRepository
-        extends JpaRepository<AssessmentResult, Long> {
-}
--- AESP-project/backend/backend/src/main/java/com/aesp/backend/repository/LearnerProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aesp.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.aesp.backend.entity.LearnerProfile;
-
-public interface LearnerProfileRepository
-        extends JpaRepository<LearnerProfile, Long> {
-}
